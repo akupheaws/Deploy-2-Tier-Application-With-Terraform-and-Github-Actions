44965909a3111080bd350bb8200313083e828423
akupheaws <akupheaws@gmail.com>
2025-07-30 16:03:47 -0500

    remove dynamodb

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,11 +6,3 @@ provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1" # For ACM and CloudFront
 }
-terraform {
-  backend "s3" {
-    bucket         = "deploy-2-tier-app-with-terraform"
-    key            = "state"
-    region         = "us-east-1"
-    dynamodb_table = "state-locking" # Optional for state locking
-  }
-}
