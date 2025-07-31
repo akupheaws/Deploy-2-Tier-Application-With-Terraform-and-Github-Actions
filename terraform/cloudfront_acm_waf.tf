8decdb66b53173cbd463d0cd96ed1d07acaa0bf6
akupheaws <akupheaws@gmail.com>
2025-07-30 19:07:21 -0500

    update code

--- terraform/cloudfront_acm_waf.tf
+++ terraform/cloudfront_acm_waf.tf
@@ -20,7 +20,7 @@ resource "aws_acm_certificate" "cert_alb" {
 
 resource "aws_route53_record" "cert_validation" {
   for_each = {
-    for dvo in setunion(aws_acm_certificate.cert_cloudfront.domain_validation_options, aws_acm_certificate.cert_alb.domain_validation_options) : dvo.domain_name => {
+    for dvo in setunion(aws_acm_certificate.cert_cloudfront.domain_validation_options, aws_acm_certificate.cert_alb.domain_validation_options) : "${dvo.domain_name}:${dvo.resource_record_name}" => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
