7be973a8927f2ad8a09fa3e6b4bac9513095cab6
akupheaws <akupheaws@gmail.com>
2025-07-30 19:10:15 -0500

    update code base

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,3 +6,12 @@ provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1" # For ACM and CloudFront
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "deploy-2-tier-app-with-terraform"   # Replace with your bucket name
+    
+    region         = "us-east-1"                    # Your AWS region
+    
+  }
+}
