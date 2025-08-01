cabd612b989fcac150eb623e5756fe1c570abb74
akupheaws <akupheaws@gmail.com>
2025-07-31 19:11:02 -0500

    updated github actions workflow

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,25 @@
 output "rds_endpoint" {
-  value = aws_db_instance.rds.endpoint
+  description = "The connection endpoint for the RDS database instance."
+  value       = aws_db_instance.rds.endpoint
+  sensitive   = true
 }
 
 output "bastion_public_ip" {
-  value = aws_instance.bastion_host.public_ip
+  description = "Public IP address of the bastion host for SSH access."
+  value       = aws_instance.bastion_host.public_ip
 }
 
 output "web_server_1_public_ip" {
-  value = aws_instance.web_server_1.public_ip
+  description = "Public IP address of the first web server."
+  value       = aws_instance.web_server_1.public_ip
 }
 
 output "web_server_2_public_ip" {
-  value = aws_instance.web_server_2.public_ip
+  description = "Public IP address of the second web server."
+  value       = aws_instance.web_server_2.public_ip
 }
 
 output "cloudfront_domain" {
-  value = aws_cloudfront_distribution.cdn.domain_name
+  description = "The domain name of the CloudFront distribution."
+  value       = aws_cloudfront_distribution.cdn.domain_name
 }
